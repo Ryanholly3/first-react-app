da933ef7a8f2694dd3d1f614559f69604c992715
ryanholly3 <ryanholly3@gmail.com>
2018-09-18 16:05:08 -0600

    added cartItems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import CartItems from './CartItems';
+import CartItem from './CartItem';
 import Footer from './Footer';
 import './App.css';
 
@@ -10,9 +11,17 @@ class App extends Component {
       <div className="App">
         <Header />
         <CartItems />
+          <CartItem />
         <Footer />
       </div>
     );
+
+    var cartItemsList =
+    [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
   }
 }
 
