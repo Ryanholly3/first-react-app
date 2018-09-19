2a43201a9d11daf1da736a13e1dd3813af8bf669
ryanholly3 <ryanholly3@gmail.com>
2018-09-19 13:07:34 -0600

    added cartItems list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import CartItems from './CartItems';
-import CartItem from './CartItem';
 import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <Header />
         <CartItems />
-          <CartItem />
         <Footer />
       </div>
     );
-
-    var cartItemsList =
-    [
-      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-    ]
   }
 }
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
 const CartItem = () => {
-  return (
-    <div class="collection-item">
-      <div class="row">
-        <div class="col-md-8">Mediocre Iron Watch</div>
-        <div class="col-md-2">$3.99</div>
-        <div class="col-md-2">1</div>
+  var cartItemList =
+  [
+    { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+    { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+    { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+  ]
+  //list component
+  const Items = items => {
+    let itemList = cartItemList.map( item => <Item key={ item.id } itemName={ item.product.name } itemPrice={ item.product.priceInCents } itemQuantity={ item.quantity } />);
+    return itemList
+  }
+
+  //list item component
+  const Item = item => {
+    return (
+      <div className="list-group-item">
+        <div className="row">
+          <div className="col-md-8">{ item.itemName }</div>
+          <div className="col-md-2">{ item.itemPrice }</div>
+          <div className="col-md-2">{ item.itemQuantity }</div>
+        </div>
       </div>
-    </div>
+    )
+  }
+
+  return (
+    <Items />
   )
 }
 
